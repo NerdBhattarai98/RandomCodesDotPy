ce6c07ce8e413c1453d12438f1f47b43b15476b0
Anbit Bhattarai <NerdBhattarai98>
2026-09-23 14:43:16 +0545

    Add vector angle calculator and classifier code

--- Week2/Numpy.py
+++ Week2/Numpy.py
@@ -19,18 +19,42 @@ import numpy as np
 # Linf = np.linalg.norm(vector,ord=np.inf)
 # print(Linf)
 
-vector = []
-size = int(input("Enter the size of vector: "))
+# vector = []
+# size = int(input("Enter the size of vector: "))
 
-for i in range(size):
+# for i in range(size):
 
-    value = int(input("Enter the necessary value: "))
-    vector.append(value)
+#     value = int(input("Enter the necessary value: "))
+#     vector.append(value)
 
-vector = np.array(vector)
-print(vector)
-print(type(vector))
+# vector = np.array(vector)
+# print(vector)
+# print(type(vector))
 
-print("The L1 Norm of the vector is",np.linalg.norm(vector,ord = 1))
-print("The L2 Norm of the vector is",np.linalg.norm(vector,ord = 2))
-print("The Linf Norm of the vector is",np.linalg.norm(vector,ord = np.inf))
+# print("The L1 Norm of the vector is",np.linalg.norm(vector,ord = 1))
+# print("The L2 Norm of the vector is",np.linalg.norm(vector,ord = 2))
+# print("The Linf Norm of the vector is",np.linalg.norm(vector,ord = np.inf))
+
+#Distance and norm
+
+# A = np.array([3,4])
+# B = np.array([2,3])
+
+# difference = A-B
+# L2 = np.linalg.norm(difference,ord = 2)
+# L1 = np.linalg.norm(difference,ord = 1)
+
+# print(difference)
+# print(L1)
+# print(L2)
+
+#Dot Product of the Vectors
+
+u = np.array([3,-4])
+v = np.array([2,5])
+
+dotp1 = np.dot(u,v)
+print(dotp1)
+
+dotp2 = u @ v
+print(dotp2)
